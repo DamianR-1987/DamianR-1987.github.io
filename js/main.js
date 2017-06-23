7c09862e0f9e8c7b499b40107e5b5f78cac6574d
Damian Rosinski <damian.rosinski1987@gmail.com>
2017-06-23 12:28:30 +0200

    Progressbar 1

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ $(".nav a").on("click", function(){
     $("nav").find(".sr-only").remove(".sr-only");
     $(this).append("<span class='sr-only'>(current)</span>");
 });
+
+$(document).ready(function(){
+var j = 0;
+function makeProgress(){
+    var i = 0;
+    
+    while(i < 100){
+        i = i + 1;
+        $(".progress-bar[active=yes]").css("width", i + "%");
+    }
+    $(".progress-bar[active=yes]").attr("active","no");
+    console.log($(".progress-bar[active=yes]").next(".progress-bar[active=no]"));
+}
+for(j=0;j<3;j++){
+    makeProgress();
+}
+});
