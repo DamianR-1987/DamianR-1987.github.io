5da1069a214409d4593c4a67f9e307cefebe1dff
DamianR <damian.rosinski1987@gmail.com>
2017-06-24 13:55:44 +0200

    Progressbar 2

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-$(".nav a").on("click", function(){
+$(".nav a").on("click", function () {
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
     $("nav").find(".sr-only").remove(".sr-only");
     $(this).append("<span class='sr-only'>(current)</span>");
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 var j = 0;
 function makeProgress(){
     var i = 0;
-    
     while(i < 100){
         i = i + 1;
-        $(".progress-bar[active=yes]").css("width", i + "%");
+        $(".progress-bar.active").css("width", i + "%");
     }
-    $(".progress-bar[active=yes]").attr("active","no");
-    console.log($(".progress-bar[active=yes]").next(".progress-bar[active=no]"));
+    console.log($(".progress-bar.active").next("progress-bar"));
+    /*$(".progress-bar[active=yes]").attr("active","no");
+    console.log($(".progress-bar[active=yes]").next(".progress-bar[active=no]"));*/
 }
-for(j=0;j<3;j++){
+/*for(j=0;j<3;j++){
     makeProgress();
-}
+}*/
+    makeProgress();
 });
