12f21df8b4cfdfec323c7ee7976d1ce275320bbb
DamianR <damian.rosinski1987@gmail.com>
2017-06-28 16:41:36 +0200

    progressbar 3

--- js/main.js
+++ js/main.js
@@ -6,19 +6,31 @@ $(".nav a").on("click", function () {
 });
 
 $(document).ready(function () {
-var j = 0;
+
+
 function makeProgress(){
-    var i = 0;
-    while(i < 100){
-        i = i + 1;
-        $(".progress-bar.active").css("width", i + "%");
-    }
-    console.log($(".progress-bar.active").next("progress-bar"));
-    /*$(".progress-bar[active=yes]").attr("active","no");
-    console.log($(".progress-bar[active=yes]").next(".progress-bar[active=no]"));*/
+    $(".progress-bar").each(function(){
+        if($(this).is("active") == true){
+            var i = 0;
+            while(i < 100){
+            i = i + 1;
+            $(this).css("width", i + "%");
+            }
+        }
+        $(this).removeClass("active");
+       });
 }
-/*for(j=0;j<3;j++){
-    makeProgress();
-}*/
-    makeProgress();
 });
+    
+/*
+$(".progress-bar.active").each(function(i,e){
+console.log($(this).next(".progress-bar"));
+});
+
+$(".progress-bar[active=yes]").attr("active","no");
+console.log($(".progress-bar[active=yes]").next(".progress-bar[active=no]"));
+    
+for(j=0;j<3;j++){
+    makeProgress();
+}
+*/
