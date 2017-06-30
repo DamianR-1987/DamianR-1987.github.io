9df3f6359ca5c511e67959b81eaf70fea2ed515b
Damian Rosinski <damian.rosinski1987@gmail.com>
2017-06-30 12:29:26 +0200

    progressbar succes :), Slider 1

--- js/main.js
+++ js/main.js
@@ -6,8 +6,22 @@ $(".nav a").on("click", function () {
 });
 
 $(document).ready(function () {
-    $(".progress-bar").eq(0).animate({width:"100%"},500);
-    $(".progress-bar").eq(1).animate({width:"100%"},900);
-    //console.log($(".progress-bar").eq(2));
-    //console.log($(".progress-bar").eq(3));
+    
+function Slider(){
+    $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_1.png")'});
+    $(".progress-bar").eq(0).animate({width:"100%"},3000, function(){
+        $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_2.jpg")'});
+        $(".progress-bar").eq(1).animate({width:"100%"},3000, function(){
+            $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_3.jpg")'});
+            $(".progress-bar").eq(2).animate({width:"100%"},3000, function(){
+                $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_4.jpg")'});
+                $(".progress-bar").eq(3).animate({width:"100%"},3000, function(){
+                    $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_5.jpg")'});
+                });
+            });
+        });
+    });
+    //Slider(); funkcja nie może powtarzać się sama w sobie
+}
+
 });
