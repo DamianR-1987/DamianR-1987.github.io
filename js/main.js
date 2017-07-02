534200f217744267df4dd235df3e318425ca5ada
DamianR <damian.rosinski1987@gmail.com>
2017-07-02 21:22:23 +0200

    Slajder 2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,52 @@
-$(".nav a").on("click", function () {
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-    $("nav").find(".sr-only").remove(".sr-only");
-    $(this).append("<span class='sr-only'>(current)</span>");
-});
-
-$(document).ready(function () {
+var $progressBar = $(".progress-bar");
+var pbc = $(".progress").length; //progressbar count
+var pbw = $(".progress").width(); //progressbar width
+var d = 4000; //duration per slides
+var i = 0;
     
-function Slider(){
-    $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_1.png")'});
-    $(".progress-bar").eq(0).animate({width:"100%"},3000, function(){
-        $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_2.jpg")'});
-        $(".progress-bar").eq(1).animate({width:"100%"},3000, function(){
-            $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_3.jpg")'});
-            $(".progress-bar").eq(2).animate({width:"100%"},3000, function(){
-                $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_4.jpg")'});
-                $(".progress-bar").eq(3).animate({width:"100%"},3000, function(){
-                    $(".banner").css({'background-image': 'linear-gradient(to bottom, rgba(252,227,138,0.3) 0%,rgba(252,227,138,0.5) 51%,rgba(252,227,138,1) 100%), url("./img/slide_5.jpg")'});
-                });
-            });
-        });
-    });
-    //Slider(); funkcja nie może powtarzać się sama w sobie
+function progressBar(){
+    if(i == 0){
+        $progressBar.css("width","0%");
+    }
+    $progressBar.eq(i).animate({"width":pbw},d,"linear");
+}
+    
+function slider(){
+    progressBar();
+        
+    setInterval(function(){
+       if(i == pbc - 1){
+           i = 0;
+       } else {
+           i++;
+       }
+        progressBar();
+        
+        if(i == 0){
+            $("section").removeClass("slide_4");
+            $("section").addClass("slide_1");
+        } else if(i == 1){
+            $("section").removeClass("slide_1");
+            $("section").addClass("slide_2");
+        } else if(i == 2){
+            $("section").removeClass("slide_2");
+            $("section").addClass("slide_3");
+        } else if(i == 3){
+            $("section").removeClass("slide_3");
+            $("section").addClass("slide_4");
+        };
+            
+    },d);
 }
 
+function current(){
+    $(".nav a").on("click", function () {
+        $("nav").find(".sr-only").remove(".sr-only");
+        $(this).append("<span class='sr-only'>(current)</span>");
+    });
+}
+
+$(document).ready(function () {
+    slider();
+    current();
 });
