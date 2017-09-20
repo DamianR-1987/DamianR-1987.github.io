38a1196d1c0aae8d7958d1574b314e571a561968
Damian Rosinski <damian.rosinski1987@gmail.com>
2017-09-20 12:56:37 +0200

    Section ourteam 2 + function cleaning node in document

--- WWDC-Task-35/js/main.js
+++ WWDC-Task-35/js/main.js
@@ -66,9 +66,22 @@ function showShoppingCartModal(){
     
 }
 
+function clean(node) {
+	for (var n = 0; n < node.childNodes.length; n ++) {
+		var child = node.childNodes[n];
+		if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+			node.removeChild(child);
+			n --;
+		} else if(child.nodeType === 1) {
+			clean(child);
+		}
+	}
+}
+
 $(document).ready(function () {
     current();
     slider();
     searchButton();
     showShoppingCartModal();
+	clean(document);
 });
